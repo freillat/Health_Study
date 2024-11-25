11a9314c05d2833ea29005dc24c295eff0e2d665
freillat <freillat@gmail.com>
2024-11-25 14:47:47 +0000

    pushing latest folder files

---
+++ predict.py
@@ -0,0 +1,29 @@
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask('model') # give an identity to your web service
+
+def load(filename: str):
+    with open(filename, 'rb') as f_in:
+        return pickle.load(f_in)
+
+(dv, model) = load('model.bin')
+
+def predict_single(patient, dv, model):
+    X = dv.transform(patient)  ## apply the one-hot encoding feature to the patient data 
+    y_pred = model.predict(X)
+    return y_pred
+
+@app.route('/predict', methods=['POST'])  ## in order to send the customer information we need to post its data.
+def predict():
+    patient = request.get_json()  ## web services work best with json frame, So after the user post its data in json format we need to access the body of json.
+    prediction = predict_single(patient, dv, model)
+    if prediction == [1]:
+        diagnostic = 'Higher risk of diabetes'
+    else:
+        diagnostic = 'Lower risk of diabetes'
+    print(diagnostic)
+    return jsonify(diagnostic)  ## send back the data in json format to the user
+
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0', port=9696) # run the code in local machine with the debugging mode true and port 9696
--- query.py
+++ query.py
@@ -1,18 +1,16 @@
-client = {
-    "sex": 1, 
-    "age": 40, 
-    "hypertension": "success", 
-    "heart_disease": 1, 
-    "ever_married":1, 
-    "work_type": 4, 
-    "Residence_type": 1, 
-    "avg_glucose_level": 2, 
-    "bmi": 35, 
-    "smoking_status": 1
+patient = {
+    "pregnancies": 1,
+    "glucose": 130,
+    "bloodpressure": 84,
+    "skinthickness": 28,
+    "insulin": 89,
+    "bmi": 35,
+    "diabetespedigreefunction": 0.3,
+    "age": 40
     }
 
 import requests ## to use the POST method we use a library named requests
 url = 'http://localhost:9696/predict' ## this is the route we made for prediction
-response = requests.post(url, json=client) ## post the patient information in json format
+response = requests.post(url, json=patient) ## post the patient information in json format
 result = response.json() ## get the server response
 print(result)
